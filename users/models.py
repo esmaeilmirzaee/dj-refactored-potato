d01de6312267009d11f0cae407becdf2f384643e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-05-03 09:30:32 +0430

    Create Car model and set default primary key for the car model

--- users/models.py
+++ users/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, verbose_name='email address')
+    user_name = models.CharField(max_length=50, verbose_name='chosen user_name')
+    car_name = models.ManyToManyField('Car', verbose_name='the user\'s cars')
 
 
 STATUS_CHOICE = (
@@ -17,10 +18,13 @@ STATUS_CHOICE = (
 
 class Website(models.Model):
     name = models.CharField(max_length=50)
-    url = models.CharField(max_length=60)
+    url = models.CharField(max_length=60, unique=True)
     release = models.DateField()
     rating = models.IntegerField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(choices=STATUS_CHOICE, max_length=1)
 
+
+class Car(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
 
