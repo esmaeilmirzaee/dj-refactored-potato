a9e4026a3107a298f308d02531d3a9dfdbe92a6e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-05-14 15:13:19 +0430

    Simple generic view and the way to change the name of template or object

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, re_path
 
-from .views import dummy_view, post_list_view, PostView
+from .views import dummy_view, post_list_view, PostView, EntryDetailView
 
 
 app_name = 'posts'
 urlpatterns = [
-    path('', PostView.as_view(), name='entry-list'),
+    path('', EntryDetailView.as_view(), name='entry-list'),
     path('<int:id>/', dummy_view, name='entry-detail'),
     path('<int:id>/delete', dummy_view, name='entry-delete'),
     path('<int:id>/update', dummy_view, name='entry-update')
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date, datetime
-from django.views import View
+from django.views import View, generic
+from .models import Entry
 
 
 def dummy_view(request):
@@ -19,4 +20,12 @@ def post_list_view(request):
 class PostView(View):
     def get(self):
         return render(self.request, 'posts.html', {})
+
+
+# Generic class-based view
+class EntryDetailView(generic.ListView):
+    model = Entry
+    context_object_name = 'object_list'
+    template_name = 'entry_list'
+
 
